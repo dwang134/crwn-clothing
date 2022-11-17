25c572dfb4d405fe4e95d102add20abf464a4404
DEELIN WONG <dc.wang547@gmail.com>
2022-11-17 17:14:33 -0500

    navbar styling

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,13 +1,15 @@
 import {Outlet, Link} from 'react-router-dom';
+import {ReactComponent as CrwnLogo} from '../../assets/crown.svg'
+import './Navbar.scss'
 
 const Navbar:React.FC = () => {
   return (
     <>
       <div className="navigation">
         <Link className="logo-container" to='/'>
-          <div>Logo</div>
+          <CrwnLogo/>
         </Link>
-        <div className="links-container">
+        <div className="nav-links-container">
           <Link className= 'nav-link' to='/shop'>SHOP</Link>
         </div>
       </div>
