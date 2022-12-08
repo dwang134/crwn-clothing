1986bd3e23cd888d2c9709b993497c73313bb3b4
rifxtt <dc.wang547@gmail.com>
2022-12-08 18:47:19 -0500

    redux store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,6 +1,11 @@
 import {compose, createStore, applyMiddleware} from 'redux';
+import logger from 'redux-logger';
 import { rootReducer } from './root-reducer';
 
 //root-reducer: one big reducer
 
-// export const store = createStore(rootReducer)
+const middlewares = [logger];
+
+const composeEnhancers = compose(applyMiddleware(...middlewares))
+
+export const store = createStore(rootReducer, undefined, composeEnhancers)
