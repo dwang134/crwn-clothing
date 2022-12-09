685c56a0df17857558abf82e2adb0f8cb4fc4315
rifxtt <dc.wang547@gmail.com>
2022-12-08 19:21:27 -0500

    user reducer dispatch

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
 import './App.scss';
 import Home from './routes/home/Homepage'
+import { onAuthStateChanged } from 'firebase/auth'
+import React, {createContext, useState, useEffect, useReducer} from 'react'
+import { createUserDocumentFromAuth, onAuthStateChangedListener } from './utils/firebase';
+import { setCurrentUser } from './store/user/userAction';
+import {useDispatch} from 'react-redux'
+import { AppDispatch } from './store/store';
 
 const App: React.FC = () =>  {
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(()=> {
+    const unsubscribe= onAuthStateChangedListener((user: any)=> {
+        if(user){
+            createUserDocumentFromAuth(user);
+        }
+        dispatch(setCurrentUser(user))
+    });
+
+    return unsubscribe;
+ }, [dispatch])
 
   return (
     <Home/>
